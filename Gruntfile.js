9ee1de3011b5b285afce4bea14f56e061b83d7e6
Hack Reactor Students <accounts@hackreactor.com>
2016-10-13 11:25:38 -0700

    Another small change

--- Gruntfile.js
+++ Gruntfile.js
@@ -51,7 +51,7 @@ module.exports = function(grunt) {
 
     shell: {
       prodServer: {
-        command: 'git push live master'
+        command: 'git add . && git commit && git push live master'
       }
     },
   });
